2bf960322bc5a3bf255fd992f6b19b889079e7d7
César <166559528+Cesitar16@users.noreply.github.com>
2025-07-04 17:43:30 -0400

    Implementación de Metodos Hateoas en el Controller de Cupon y extend A reprentationModel en el DTO (#4)

--- src/main/java/com/edutech/cupones/dto/CuponDTO.java
+++ src/main/java/com/edutech/cupones/dto/CuponDTO.java
@@ -2,10 +2,12 @@ package com.edutech.cupones.dto;
 
 import java.time.LocalDate;
 
+import org.springframework.hateoas.RepresentationModel;
+
 import lombok.Data;
 
 @Data
-public class CuponDTO {
+public class CuponDTO extends RepresentationModel <CuponDTO>{
 
     private Integer idCupon;
     private String codigo;
